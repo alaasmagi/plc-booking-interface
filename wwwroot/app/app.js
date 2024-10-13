955a0559d752cb9ba90a80f9e5501e2c83c7a812
Aleksander Laasmägi <allaas@taltech.ee>
2024-10-13 15:13:10 +0300

    Added javascript to date selection

--- wwwroot/app/app.js
+++ wwwroot/app/app.js
@@ -1,6 +1,18 @@
-﻿namespace plc_booking_interface.wwwroot.app
-{
-    public class app
-    {
-    }
+﻿function updateTime() {
+    const slider = document.getElementById("timeRange");
+    const selectedTimeStartDisplay = document.getElementById("selectedTimeStart");
+    const selectedTimeEndDisplay = document.getElementById("selectedTimeEnd");
+
+    const startHour = Math.floor(slider.value / 2); 
+    const startMinutes = (slider.value % 2) * 30; 
+
+    const endValue = Math.min(Number(slider.value) + 1, 48); 
+    const endHour = Math.floor(endValue / 2);
+    const endMinutes = (endValue % 2) * 30; 
+
+    const formattedStartTime = `${String(startHour).padStart(2, '0')}:${String(startMinutes).padStart(2, '0')}`;
+
+    const formattedEndTime = `${String(endHour).padStart(2, '0')}:${String(endMinutes).padStart(2, '0')}`;
+    selectedTimeStartDisplay.textContent = formattedStartTime;
+    selectedTimeEndDisplay.textContent = formattedEndTime;
 }
